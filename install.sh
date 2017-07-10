086a0d9c674d3fd9ba352a3c1b4bebc25671d5db
Fidel Salgado <fidelsalgado30@gmail.com>
2017-07-09 19:54:31 -0700

    Adding bash_profile, inputrc, and gitconfig

---
+++ install.sh
@@ -0,0 +1,16 @@
+# ====== Fidel's Dotfiles Install Script ====== 
+
+# "${BASH_SOURCE[0]}" returns the path of this file. Then dirname takes
+# this path and strips the filename to get the dotfiles directory. We change
+# directory to the dotfiles directory and then use pwd to assign the dotfiles
+# path to the DOTFILES_DIR environment variable.
+# TL;DR: Gets the current directory and sets it to DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Set symlinks for all dotfiles
+ln -sfv "$DOTFILES_DIR/bash_profile" ~/.bash_profile
+ln -sfv "$DOTFILES_DIR/inputrc" ~/.inputrc
+ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
+
+# Cool message
+echo "Done installing!"
